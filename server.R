67329b14168b20dd2232fc40da2324e75d61e493
langdav <56968651+langdav@users.noreply.github.com>
2021-03-12 12:18:07 +0100

    Update server.R

--- server.R
+++ server.R
@@ -7,23 +7,32 @@ source("global.R")
 
 shinyServer(function(input, output, session) {
   
-  output$selected_var <- renderText({
-    
-    paste("Area of interest: blablabla, Italy")
-  })
-  
-  points <- eventReactive(input$recalc, {
-    cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
-  }, ignoreNULL = FALSE)
-  
+  # output$image1 <- renderPlot({
+  #   if(!is.null(input$map_marker_click)){
+  #     op <- par(mar = rep(0, 4))
+  #     plot(NULL, xlim = c(0, 100), ylim = c(0, 100), bty = "n", axes = F)
+  #     text(x = 50, y = 50, "F\u00fcr diese Urzeit liegen an \ndiesem Baum leider keine Daten vor.", cex = 2, col = "black")
+  #     par(op)
+  #   }
+  #  
+  # }, width = 400, height = 400)
   
   output$map <- renderLeaflet({
-    leaflet() %>%
-      addProviderTiles(providers$Esri.WorldImagery,
-                       options = providerTileOptions(noWrap = TRUE)
-       ) %>%
-      #addTiles() %>%
-       addMarkers(data = cbind(1.660626,42.099371))
+    if(is.null(input$map_marker_click)){
+      leaflet() %>%
+        addProviderTiles(providers$OpenStreetMap,
+                         options = providerTileOptions(noWrap = TRUE)) %>%
+        addMarkers(data = cbind(1.660626,42.099371), popup = "CLICK ME") %>%
+        #addPopups(1.660626,42.099371, "Pont of interest") %>%
+        #fitBounds(0, 40, 10, 50) %>%
+        setView(1.660626,42.099371, zoom = 5)
+    } else {
+      leaflet() %>%
+        addProviderTiles(providers$Esri.WorldImagery,
+                         options = providerTileOptions(noWrap = TRUE)) %>%
+        addMarkers(data = cbind(1.660626,42.099371)) %>%
+        setView(1.660626,42.099371, zoom = 17)
+    }
   })
   
   output$dyTemp <- renderDygraph({
